d349eb0d6e24d98833888ac643f7122f78148501
Edvin <edvin.linge@gmail.com>
2026-07-27 22:53:41 +0200

    Version 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,15 +11,16 @@ android {
         applicationId = "com.edvinlinge.hemma.mathstars2"
         minSdk = 29
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
